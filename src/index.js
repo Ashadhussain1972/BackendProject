1fbf3b523ef42fe6c0311ce6ae3a56699c872763
Ashadhussain1972 <ashad1972.ah@gmail.com>
2025-03-17 14:51:40 +0530

    Utils Updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
-import connectDB from "./DB/db.js";
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants.js';
+import connectDB from './DB/db.js';
+import app from './app.js'; 
 
 // Load environment variables
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: './.env' });
 
 // Connect to Database
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log('MongoDB Connection Failed:', error);
+  });
