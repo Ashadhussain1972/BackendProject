bc944f56cb359088f333e2bd09f8196e8f9254d3
Ashadhussain1972 <ashad1972.ah@gmail.com>
2025-06-28 18:39:13 +0530

    Add User and Video models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 
 import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema({
     username:{
@@ -46,4 +48,41 @@ const userSchema = new Schema({
     },
 },{timestamps: true});
 
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next();
+
+        this.password = await bcrypt.hash(this.password, 10);
+        next();
+    
+
+        userSchema.methods.comparePassword = async function (password) {
+            return await bcrypt.compare(password, this.password);
+        }
+        userSchema.methods.generateAccessToken = function () {
+            jwt.sign({
+                _id : this._id,
+                email: this.email,
+                username: this.username,
+                fullName: this.fullName,
+            },
+            process.env.JWT_ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN
+            }
+        )
+        }
+        userSchema.models.generateRefreshToken = function () {
+            jwt.sign({
+                _id : this._id,
+                email: this.email,
+                username: this.username,
+                fullName: this.fullName,
+            },
+            process.env.JWT_REFRESH_TOKEN_SECRET,
+            {
+                expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRES_IN
+            })
+        }
+})
 export const User = mongoose.model("User", userSchema);  
